78c68fe86f50cdfe33bba27644112400f699ee4c
nikhil <nikhil@gmail.com>
2018-06-10 22:57:31 +0530

    change1

--- java1/src/com/nt/java/First1.java
+++ java1/src/com/nt/java/First1.java
@@ -5,8 +5,10 @@ import java.util.Date;
 public class First1 {
 	public static void main(String args[])
 	{
+		wish w=new wish();
 		System.out.println("welcome to github");
 		System.out.println("the date is"+new Date());
+	   System.out.println("the wish-->"+w.wish("raja"));
 	}
 
 }
---
+++ java1/src/com/nt/java/wish.java
@@ -0,0 +1,9 @@
+package com.nt.java;
+
+public class wish {
+	public String wish(String user)
+	{
+		return "good morning"+user;
+	}
+
+}
