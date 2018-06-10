51f78f3ca79c1f90122578dee9ab1c2ea1cbf75c
nikhil <nikhil@gmail.com>
2018-06-10 22:42:37 +0530

    change4

--- java1/src/com/nt/java/First1.java
+++ java1/src/com/nt/java/First1.java
@@ -1,10 +1,12 @@
 package com.nt.java;
 
+import java.util.Date;
+
 public class First1 {
 	public static void main(String args[])
 	{
 		System.out.println("welcome to github");
-		
+		System.out.println("the date is"+new Date());
 	}
 
 }
